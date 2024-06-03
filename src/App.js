76a8850b1fed92f970ddf84ea580ec0565e02dee
ctrevera <carlos_trevera_davila@homedepot.com>
2024-06-03 14:25:14 -0600

    first commit dont move nothing here

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DragAndDrop from './DragAndDrop';
+
+
+function App() {
+    return (
+        <div className="App">
+            <h1>test</h1>
+            <DragAndDrop />
+        </div>
+    );
+}
+export default App;
--- src/initial-data.js
+++ src/initial-data.js
@@ -1,21 +1,31 @@
 const initialData = {
-
     tasks: {
-        'task-1': { id: 'task-1', content: 'take out the trash' },
-        'task-2': { id: 'task-2', content: 'watch tv ' },
-        'task-3': { id: 'task-3', content: 'charge my phone' },
-        'task-4': { id: 'task-4', content: 'cook dinner' },
-
+        'task-1': { id: 'task-1', content: 'Take out the garbagesdasdsadas' },
+        'task-2': { id: 'task-2', content: 'Watch my favorite show' },
+        'task-3': { id: 'task-3', content: 'Charge my phone' },
+        'task-4': { id: 'task-4', content: 'Cook dinner' },
+        'task-5': { id: 'task-5', content: 'Cook breakfast' },
+        'task-6': { id: 'task-6', content: 'Cook meals' },
     },
     columns: {
         'column-1': {
             id: 'column-1',
             title: 'To do',
             taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
-
-        }
+        },
+        'column-2': {
+            id: 'column-2',
+            title: 'In progress',
+            taskIds: ['task-5'],
+        },
+        'column-3': {
+            id: 'column-3',
+            title: 'Finish ',
+            taskIds: ['task-6'],
+        },
     },
-    columnOrder: ['column-1'],
-
+    // Facilitate reordering of the columns
+    columnOrder: ['column-1', 'column-2', 'column-3'],
 };
+
 export default initialData;
